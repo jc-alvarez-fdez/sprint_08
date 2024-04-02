806db658d893f2718470f004929ab2b0275f3a30
jc-alvarez-fdez <jc.alvarez.fdez@gmail.com>
2024-04-02 17:51:25 +0200

    solución conexión server

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('sprint_08_db', 'root', '', {
+const sequelize = new Sequelize('sprint08_db', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
   });
